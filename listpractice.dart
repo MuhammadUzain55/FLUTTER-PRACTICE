e449ec887f2d80d0e0b9af51f01df56ce2e2d327
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-10 19:32:56 +0500

    List practice in dart

---
+++ listpractice.dart
@@ -0,0 +1,83 @@
+
+
+void main() {
+//   List studentNames = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   studentNames.replaceRange(1, 3, ["fareed", "haseeb"]);
+//   print(studentNames);
+
+// List studentNames = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+// studentNames.replaceRange(studentNames.length -1, studentNames.length , ["fareed", "haseeb"]);
+// print(studentNames);
+
+//   List studentROllNum = [45, 33, 67, 86, 90, 19, 23, 61, 52];
+//   studentROllNum.sort();
+//   print(studentROllNum);
+
+//  List studentROllNum = ["asad", "bilal", "omer", "Uzair", "Sheraz", "Abid", "khizer", "faraz", "shazaib"];
+//  studentROllNum.sort();
+//  print(studentROllNum);
+
+//   List studentROllNum = [
+//     "asad",
+//     "bilal",
+//     "omer",
+//     "Uzair",
+//     "Sheraz",
+//     "Abid",
+//     "khizer",
+//     "faraz",
+//     "shazaib"
+//   ];
+//   studentROllNum.sort();
+//   print(studentROllNum);
+//   var abc = List.of(studentROllNum.reversed);
+//   print(abc);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   print(names.isEmpty);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.clear();
+//   print(names);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   print(names.isEmpty);
+//   names.clear();
+//   print(names.isNotEmpty);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.add("shakir");
+//   print(names);
+
+//  List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.addAll(["shakir", "fakir"]);
+//   print(names);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.insert(3, "shehbaz");
+//   print(names);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   var abc = names.reversed;
+//   print(abc);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   var abc = List.of(names.reversed);
+//   print(abc);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.remove("omer");
+//   print(names);
+
+//   List names = ["asad", "bilal", "omer", "khizer", "faraz", "shazaib"];
+//   names.removeAt(1);
+//   print(names);
+
+  // List num = [5, 8, 9, 3, 7, 2, 10];
+  // num.removeWhere((u) => u > 5);
+  // print(num);
+
+//  List num = [5, 8, 9, 3, 7, 2, 10];
+//   num.retainWhere((u) => u > 5);
+//   print(num);
+}
