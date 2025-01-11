f0d589dc69902d9cc1c1ddc2352a1d866563b848
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-11 16:39:57 +0500

    Map practice in dart

---
+++ mapPractice.dart
@@ -0,0 +1,61 @@
+void main() {
+//   Map studentRecords = {"name": "Bilal", "rollNo": 3021};
+//   print(studentRecords);
+
+// Map studentRecords = {"name": "Bilal", "rollNo": 3021};
+//   print(studentRecords.keys);
+
+// Map studentRecords = {"name" : "Bilal", "rollNo": "3021"};
+//   print(studentRecords["name"]);
+
+//   Map student1Records = {
+//     "name": ["Bilal", "Shehbaz"],
+//     "rollNo": [3021, 3022]
+//   };
+//   print(student1Records["name"][0]);
+//   print(student1Records["rollNO"][0]);
+
+  // List students = [
+  //   {"name": "Bilal", "rollNo": 3021},
+  //   {"name": "Shehbaz", "rollNo": 3022}
+  // ];
+  // print(students[0]);
+
+  // size
+  // Map students = {"name": "Bilal", "rollNo": 3021, "marks": 67};
+  // print(students.length);
+
+  // add & update
+  // Map fruit = {1: "Apple", 2: "Mango"};
+  // fruit[3] = "Banana";
+  // print(fruit);
+
+  // jo key exist nhi krti add krta hai
+  // Map fruit = {1: "Apple", 2: "Mango"};
+  // fruit.putIfAbsent(3,() => "Banana");
+  // print(fruit);
+
+  // remove
+  // Map fruit = {1: "Apple", 2: "Mango"};
+  // fruit.remove(1);
+  // print(fruit);
+
+  // practice
+  // List haha = [
+  //   {
+  //     0: 0,
+  //     1: [1],
+  //   },
+  //   {"2": "2"},
+  //   {
+  //     1: {
+  //       "hehe": [
+  //         {
+  //           "hehe": {"haha": "bye"}
+  //         }
+  //       ]
+  //     }
+  //   }
+  // ];
+  // print(haha[2][1]["hehe"][0]["hehe"]["haha"]);
+}
