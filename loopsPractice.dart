00ce124a44543b11983f8938ffc9be722a37db96
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-01-12 17:38:59 +0500

    Loops practice

---
+++ loopsPractice.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main() {
+// pre increment ++a
+  // var abc = 0;
+  // abc = ++abc;
+  // print(abc);
+
+  // post increment a++
+  // var abc = 0;
+  // abc = abc++;
+  // print(abc);
+
+// pre decrement --a
+  // var abc = 0;
+  // abc = --abc;
+  // print(abc);
+
+
+
+}
+
